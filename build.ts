2db7556a4b823f783b517049a80c493ecaa50473
jollyroger182 <hello@jollyy.dev>
2026-04-02 19:28:37 +0800

    initial commit

---
+++ build.ts
@@ -0,0 +1,22 @@
+import 'bun'
+import { rm } from 'node:fs/promises'
+
+await rm('./dist', { recursive: true, force: true })
+
+await Bun.build({
+	entrypoints: ['./src/index.ts'],
+	outdir: './dist',
+	target: 'node',
+	format: 'esm',
+})
+
+await Bun.build({
+	entrypoints: ['./src/index.ts'],
+	outdir: './dist',
+	target: 'node',
+	format: 'cjs',
+	naming: '[name].cjs',
+})
+
+const proc = Bun.spawnSync(['bunx', 'tsc', '-p', 'tsconfig.build.json'])
+if (!proc.success) process.exit(1)
