a3d4ebeb6aff060d79137c121f466cf061c878d0
jollyroger182 <hello@jollyy.dev>
2026-04-04 17:05:23 +0800

    chore: use tsc for building

--- build.ts
+++ build.ts
@@ -3,21 +3,6 @@ import { rm } from 'node:fs/promises'
 
 await rm('./dist', { recursive: true, force: true })
 
-await Bun.build({
-	entrypoints: ['./src/index.ts'],
-	outdir: './dist',
-	target: 'node',
-	format: 'esm',
-})
-
-await Bun.build({
-	entrypoints: ['./src/index.ts'],
-	outdir: './dist',
-	target: 'node',
-	format: 'cjs',
-	naming: '[name].cjs',
-})
-
 const proc = Bun.spawnSync(['bunx', 'tsc', '-p', 'tsconfig.build.json'], {
 	stdout: 'inherit',
 	stderr: 'inherit',
