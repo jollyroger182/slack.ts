f8e18a8f2021b31210e83d195f84771ed5caa00b
jollyroger182 <hello@jollyy.dev>
2026-04-05 17:56:23 +0800

    fix: unify env var names

--- examples/basic.ts
+++ examples/basic.ts
@@ -1,6 +1,6 @@
 import { App } from '../src'
 
-const app = new App({ token: process.env.SLACK_TOKEN! })
+const app = new App({ token: process.env.SLACK_BOT_TOKEN! })
 
 const channel = app.channel(process.env.SLACK_CHANNEL!)
 const message = await channel.send({ text: 'hello world' })
