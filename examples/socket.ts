a05044e4ddb02f859b85b499fcc44ce30b649d15
jollyroger182 <hello@jollyy.dev>
2026-04-05 16:42:13 +0800

    feat: add socket mode example

---
+++ examples/socket.ts
@@ -0,0 +1,13 @@
+import { App } from 'slack.ts'
+
+const app = new App({
+	token: process.env.SLACK_BOT_TOKEN,
+	receiver: { type: 'socket', appToken: process.env.SLACK_APP_TOKEN! },
+})
+
+app.message(async ({ message }) => {
+	if (message.user === process.env.SLACK_USER_ID) return
+	await message.reply("I'm always listening :eyes:")
+})
+
+await app.start()
